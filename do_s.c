4086d968f6a317e830e939db2f0ed800ec80fd8b
GJLeetrainer <jayleeannvelez@yahoo.com>
2024-12-03 03:55:36 -0400

    string specifier

---
+++ do_s.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * do_s - Does the %s format specifier for strings.
+ * @args: The list of variable arguments passed to _printf.
+ *
+ * Return: The number of characters printed.
+ */
+int do_s(va_list args)
+{
+	char *p = va_arg(args, char *);
+	int count = 0;
+
+	if (!p)
+		p = "(null)";
+
+	while (*p)
+	{
+		count += _putchar(*p);
+		p++;
+	}
+	return (count);
+}
