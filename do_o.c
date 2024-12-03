eb732c5ecbf2f3e794d31c6bec14c8ff352b5b77
GJLeetrainer <jayleeannvelez@yahoo.com>
2024-12-03 09:17:48 -0400

    complying to betty

--- do_o.c
+++ do_o.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * manage_o - Does the %o specifier for octal numbers.
+ * do_o - Does the %o specifier for octal numbers.
  * @args: The list of arguments.
  *
  * Return: The number of characters printed.
  */
+
 int do_o(va_list args)
+
 {
 	unsigned int num = va_arg(args, unsigned int);
 	char buffer[20];
 	int count = 0;
 	int i = 0;
-	transform_base(num, 8, buffer, 0);
 
+	transform_base(num, 8, buffer, 0);
 	for (i = 0; buffer[i] != '\0'; i++)
 		count += _putchar(buffer[i]);
 
