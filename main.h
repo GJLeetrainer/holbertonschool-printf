fc6d30d2d7167b536be9cfbbc285f6615c66cc94
GJLeetrainer <jayleeannvelez@yahoo.com>
2024-12-03 03:37:04 -0400

    header

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stddef.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int d_terminator_specifier(const char *format, va_list args);
+int do_c(va_list args);
+int do_s(va_list args);
+int do_di(va_list args);
+int do_u(va_list args);
+int do_o(va_list args);
+int do_p(va_list args);
+int do_x(va_list args, int is_uppercase);
+void transform_base(unsigned int num, int base, char *buffer, int is_uppercase);
+
+#endif
